a4b5496821c4c60509094b4e115c8c8d828ccb3f
Brittany Blake <brittany.blake49@gmail.com>
2020-07-10 14:51:15 +0200

    Add book model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_103653) do
+ActiveRecord::Schema.define(version: 2020_07_10_124231) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_of_pages"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
