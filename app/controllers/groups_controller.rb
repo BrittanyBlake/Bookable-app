5862f794ee7c58a72d1b54b1a85d2864d9b4d4d1
Brittany Blake <brittany.blake49@gmail.com>
2020-07-11 14:52:27 +0200

    Start group show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,10 @@ class GroupsController < ApplicationController
         end    
     end
 
+    def show
+        @group = Group.find(params[:id])
+    end
+
     private
     def group_params
         params.require(:group).permit(:name)
