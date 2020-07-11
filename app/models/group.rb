2233fb9bd711b495eca94d68a67ca1ffa57e1dc5
Brittany Blake <brittany.blake49@gmail.com>
2020-07-11 14:39:50 +0200

    Add validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ApplicationRecord
     has_many :book_groups
     has_many :books, through: :book_groups
     belongs_to :user
+    validates :name, presence: true, length: {minimum: 3, maximum: 30}
     scope :ordered_alphabetically, -> { order(name: :asc) }
 end
