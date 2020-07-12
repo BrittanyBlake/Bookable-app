c39a88dd96e94177012ffe6b9acd48d085ae10c0
Brittany Blake <brittany.blake49@gmail.com>
2020-07-12 20:35:37 +0200

    re do navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery3
 //= require bootstrap
+//= require bootstrap-sprockets
+//= require jquery3
 //= require activestorage
 //= require turbolinks
 //= require_tree .
