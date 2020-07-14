773b5dda8e7946baa981fa8118014185b1ae7a24
Brittany Blake <brittany.blake49@gmail.com>
2020-07-14 10:51:52 +0200

    Fix bootstrap dismissible messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
 //= require bootstrap
 //= require bootstrap-sprockets
-//= require jquery3
 //= require activestorage
 //= require turbolinks
 //= require_tree .
