88e1ee0d56c24c036cc4cc19949b433638475cb9
Brittany Blake <brittany.blake49@gmail.com>
2020-07-14 16:23:50 +0200

    Add specs for book_group model

---
+++ spec/factories/book_groups.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :book_group do
+    
+  end
+end
---
+++ spec/models/book_group_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe BookGroup, type: :model do
+ describe 'associations' do
+    it { should belong_to(:book) }
+    it { should belong_to(:group) }
+  end
+end
