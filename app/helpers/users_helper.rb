e7de5ae358c0017d8b8aa3109bda02c9d63254f2
Brittany Blake <brittany.blake49@gmail.com>
2020-07-15 14:57:14 +0200

    remove logic from user profile page and add default image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def check_avatar(user)
+    if @user.image_url.nil?
+      image_tag('default.jpeg', alt: user.username, class: 'avatar')
+    else
+      image_tag(@user.image.url, alt: user.username, class: 'avatar')
+    end
+  end
 end
