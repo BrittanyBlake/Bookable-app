2ad05f1b409ca83d1accbd7d4e0a0ac852ad67aa
Brittany Blake <brittany.blake49@gmail.com>
2020-07-15 23:48:15 +0200

    fix heroku issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  BOOKABLEAPP::Application.configure do
+     config.assets.compile = true
+end
 end
