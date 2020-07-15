34c41d977d281464e678c8b494263c6476d0f3b3
Brittany Blake <brittany.blake49@gmail.com>
2020-07-15 15:23:04 +0200

    remove logic from nav and add back button to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def signed_in
+        if current_user
+            render 'layouts/usernav' 
+        else
+            render 'layouts/navigation'
+        end
+    end
 end
